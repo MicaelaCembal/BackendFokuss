fbd627a3a31403ec3d2ce0043cff5733e2a0938d
Micaela Cembal <micaelacembal@gmail.com>
2026-06-09 14:14:14 -0300

    cambio variable

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,6 @@ const nodemailer = require('nodemailer');
 
 const User = require('../models/User');
 
-const nodemailer = require('nodemailer');
-
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
