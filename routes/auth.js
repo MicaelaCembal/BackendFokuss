01a630c2dd09849fba8b25efe9fd37dcc26f07ee
Micaela Cembal <micaelacembal@gmail.com>
2026-06-09 14:20:32 -0300

    logs para render

--- routes/auth.js
+++ routes/auth.js
@@ -7,18 +7,18 @@ const nodemailer = require('nodemailer');
 const User = require('../models/User');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
   host: 'smtp.gmail.com',
-  port: 587,           
-  secure: false,      
+  port: 465,         
+  secure: true,     
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS, 
   },
-  tls: {
-    rejectUnauthorized: false 
-  }
+  debug: true,       
+  logger: true   
 });
+
+
 const verificarToken = (req, res, next) => {
 	try {
 		const authHeader = req.headers.authorization;
